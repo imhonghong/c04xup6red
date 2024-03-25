ea66f5763d71e385036b38b0b9cb23712361134a
Han Lin, Hung <131515657+imhonghong@users.noreply.github.com>
2024-03-26 03:14:44 +0800

    Update and rename VLSI_measure_current.py to VLSI_HW3_cmd.py

--- VLSI_HW3_cmd.py
+++ VLSI_HW3_cmd.py
@@ -1,3 +1,22 @@
+# This part is  editing sp file, replace .PROBE.... by the comment below
+#modify (in),(out)...by your pin lable
+'''
+.param vdd=1.8
+.PROBE TRAN
++    I(c0)
++    I(c0)
++    V(out)
++    V(in)
+.TRAN 1e-9 100e-9 START=0.0
+.meas TRAN Trise TRIG V(in) VAL='0.9' RISE=2
++    TARG V(out) VAL='0.9' RISE=2
+.meas TRAN Tfall TRIG V(in) VAL='0.9' FALL=2
++    TARG V(out) VAL='0.9' FALL=2
+
+.meas TRAN Ic0 max I(c0)
+'''
+
+# This part is generating .alter case to simulate 45PVT corner
 # note that when you run this program, 
 # you should clean the txt file first,
 # otherwise the data will be appended to the file
@@ -12,7 +31,6 @@ f = open(path, 'w')
 for i in p:
     for j in vdd:
         for k in TEMP:
-            print(".meas tran Ic0 max I(c0)",file=f)
             print(f".alter {i} {j} {k}",file=f)
             #Second ,vt11330 -> your workstation name
             print(r'.LIB "/home/vlsi113_TJ/vt11330/UM180FDKMFC00000OA_B02/Models/Hspice/mm180_reg18_v124.lib"',i,file=f)
